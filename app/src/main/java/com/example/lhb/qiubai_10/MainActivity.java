e283f8839d939fded3f043df2948173b55632bcf
lhb <6245478@qq.com>
2016-11-07 18:51:43 +0800

    1107night

--- app/src/main/java/com/example/lhb/qiubai_10/MainActivity.java
+++ app/src/main/java/com/example/lhb/qiubai_10/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.lhb.qiubai_10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        radioGroup = ((RadioGroup) findViewById(R.id.main_bottom_group));
     }
 }
